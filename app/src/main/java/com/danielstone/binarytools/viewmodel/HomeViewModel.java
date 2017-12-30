f1c8fcae187d8fa2cf0b7977bbf150e0d8a2f44b
Daniel Stone <danielstone411@gmail.com>
2017-12-30 17:22:38 +0000

    Wrote the algorithm to convert from any base to base 10 & from base 10 to any base using BigInteger so massive numbers can be converted

--- app/src/main/java/com/danielstone/binarytools/viewmodel/HomeViewModel.java
+++ app/src/main/java/com/danielstone/binarytools/viewmodel/HomeViewModel.java
@@ -4,9 +4,7 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
-/**
- * Created by danielstone on 28/12/2017.
- */
+import java.math.BigInteger;
 
 public class HomeViewModel extends ViewModel {
 
@@ -30,20 +28,59 @@ public class HomeViewModel extends ViewModel {
 
     public boolean setNewValue(String s, int radix) {
         try {
-            Long l = Long.parseLong(s, radix);
-//            if (radix != 2) baseTwo.setValue(Long.toString(l, 2));
-            baseTwo.setValue(Long.toString(l, 2));
-            baseEight.setValue(Long.toString(l, 8));
-            baseTen.setValue(Long.toString(l, 10));
-            baseSixteen.setValue(Long.toString(l, 16).toUpperCase());
-            baseN1.setValue(Long.toString(l, n1Base).toUpperCase());
-            baseN2.setValue(Long.toString(l, n2Base).toUpperCase());
+            s = s.toUpperCase();
+            // Convert from base radix to base 10.
+            BigInteger base = BigInteger.valueOf(radix);
+            BigInteger result = new BigInteger("0");
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i); // the
+                int u = (int) c; // integer representation of the character in ascii
+                if ((u >= '0' && u <= '9') || (u >= 'A' && u <= 'Z')) { // if the character is a number or alphabetical letter
+                    if ((u >= '0' && u <= '9')) u = u - '0';
+                    else u = (u - 'A') + 10; // change the integer representation to the correct value
+                    BigInteger digit = new BigInteger(String.valueOf(u));
+                    BigInteger value = digit.multiply(new BigInteger(String.valueOf(base.pow(s.length() - (i + 1)))));
+                    result = result.add(value);
+                } else {
+                    throw new Exception();
+                }
+            }
+
+            baseTwo.setValue(convertToBase(result, 2));
+            baseEight.setValue(convertToBase(result, 8));
+            baseTen.setValue(convertToBase(result, 10));
+            baseSixteen.setValue(convertToBase(result, 16));
+            baseN1.setValue(convertToBase(result, n1Base));
+            baseN2.setValue(convertToBase(result, n2Base));
+
             return true;
 
-        } catch (Exception e) {
+        } catch (Throwable e) {
+            if (radix != 2) baseTwo.setValue("");
+            if (radix != 8) baseEight.setValue("");
+            if (radix != 10) baseTen.setValue("");
+            if (radix != 16) baseSixteen.setValue("");
+            if (radix != n1Base) baseN1.setValue("");
+            if (radix != n2Base) baseN2.setValue("");
             return false;
         }
 
+    }
+
+    private String convertToBase(BigInteger decimal, int radix) throws Throwable {
+        String result = "";
+        while (decimal.intValue() != 0) {
+            BigInteger[] division = decimal.divideAndRemainder(BigInteger.valueOf(radix));
+            int remainder = division[1].intValue();
+            char c = (char) ('0' + remainder); // get the character using the remainder
+            if (remainder > 9) { // if the character
+                int u = remainder - 10; // find the index of the alphabetical letter
+                c = (char) ('A' + u); // update the character
+            }
+            result = c + result; // append to beginning of result
+            decimal = division[0]; // use the new decimal value.
+        }
+        return result;
     }
 
     public LiveData<String> getBaseTwo() {
